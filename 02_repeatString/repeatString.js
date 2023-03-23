736074ab1ce064ae9b5a995b3edaa34fa53d304a
Ash <ajmwilliams85@gmail.com>
2023-03-23 16:31:28 +0000

    Exercise 01 and 02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeatedString = "";
 
+  if (num < 0) return "ERROR";
+  for (i = 0; i < num; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
+
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
