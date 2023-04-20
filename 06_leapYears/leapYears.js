fc35e51cc491c0b4184086aa2304ead7b0ec0569
Ash <ajmwilliams85@gmail.com>
2023-04-20 22:12:19 +0100

    Exercise 06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let leapYear = Boolean(true);
 
+  if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+    console.log("Leap Year");
+    return leapYear;
+  } else if (year % 100 === 0 || (year % 4 != 0 && year % 400 != 0)) {
+    console.log("Not a Leap Year");
+    return (leapYear = false);
+  }
 };
+
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
