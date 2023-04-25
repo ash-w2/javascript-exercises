e6a78c5080c3ebddeb2552cf765a5e45d48812b7
Ash <ajmwilliams85@gmail.com>
2023-04-25 22:25:53 +0100

    Exercise 09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[!., ]/g, "");
+  const reverseStr = str.split("").reverse("").join("").replace(/[!., ]/g, "");
 
+  if (str == reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
