5f104a22e2b2f20120d54027790c3f2f03a02496
Ash <ajmwilliams85@gmail.com>
2023-05-11 21:16:31 +0100

    Exercise 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (books) {
+  //return title.bookName;
+  return books.map((item) => item.title);
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+//find item(title) of each element in an array(books)
+//return the items into a new array
